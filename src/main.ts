47051c92dd1600af32e7c873ec7a6b403b4f791e
Alan934 <sanjurjoalan77@gmail.com>
2024-10-09 16:54:38 -0300

    update deploy

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { envs } from './config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:3000', '*'],
   });
   app.setGlobalPrefix('api');
 
