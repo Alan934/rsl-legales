f3043431409eef76528cc829f769524ab9cc51bb
Alan934 <sanjurjoalan77@gmail.com>
2024-10-09 16:43:30 -0300

    update deploy

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { envs } from './config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: 'localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
