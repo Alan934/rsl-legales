15347b171b5182dbc55861e807dd1ea276ca5613
Alan934 <sanjurjoalan77@gmail.com>
2024-10-21 19:29:20 -0300

    add ip controller

--- src/ip/ip.controller.ts
+++ src/ip/ip.controller.ts
@@ -1,23 +1,30 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, UseGuards } from '@nestjs/common';
-import { AuthGuard } from 'src/auth/guards';
+import { Controller, Post, Body, Logger, BadRequestException } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { IpService } from './ip.service';
 
 @ApiTags('Ips')
 @Controller('ip')
 export class IpController {
+    private readonly logger = new Logger('IpController');
+
     constructor(private readonly ipService: IpService) {}
 
     @Post()
     async saveIp(@Body() body: { ip: string }) {
-      const { ip } = body;
-  
-      if (!ip) {
-        throw new Error('IP is required');
-      }
-  
-      await this.ipService.saveIp(ip);
-      return { message: 'IP guardada correctamente' };
-    }
+        const { ip } = body;
 
+        if (!ip) {
+            this.logger.error('No IP provided in the request body');
+            throw new BadRequestException('IP is required');
+        }
+
+        try {
+            this.logger.log(`Saving IP: ${ip}`);
+            await this.ipService.saveIp(ip);
+            return { message: 'IP guardada correctamente' };
+        } catch (error) {
+            this.logger.error(`Error al guardar la IP: ${error.message}`);
+            throw new BadRequestException(`Error al guardar la IP: ${error.message}`);
+        }
+    }
 }
