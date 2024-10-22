f9517de1fde2318c0a7e88f72871a7beba0be11b
Alan934 <sanjurjoalan77@gmail.com>
2024-10-22 13:34:43 -0300

    update create ip.dto

--- src/ip/dto/create-ip.dto.ts
+++ src/ip/dto/create-ip.dto.ts
@@ -1,8 +1,10 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 
 export class CreateIpDto {
     
-
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
     ip?: string;
 }
